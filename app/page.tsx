5ccf77eec9b66af9f13b140623b0323aa6389078
linhcu1612 <nhatlinh161232@gmail.com>
2023-03-05 16:53:04 +1100

    build: add in test library

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from 'next/image';
+import {Roboto} from 'next/font/google';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({subsets: ['latin'], weight: ['400', '500', '700']});
 
 export default function Home() {
   return (
@@ -52,10 +52,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
+          <h2 className={roboto.className}>
             Docs <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
+          <p className={roboto.className}>
             Find in-depth information about Next.js features and API.
           </p>
         </a>
@@ -66,10 +66,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
+          <h2 className={roboto.className}>
             Templates <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
+          <p className={roboto.className}>Explore the Next.js 13 playground.</p>
         </a>
 
         <a
@@ -78,14 +78,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
+          <h2 className={roboto.className}>
             Deploy <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>
+          <p className={roboto.className}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
